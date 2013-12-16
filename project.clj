4d885bd49338f398095ea134764e4d23c80d4137
Matthew Gilliard <matthew.gilliard@gmail.com>
2013-12-16 22:29:50 +0000

    After some effort, we're off the ground

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clepl "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.jboss.aesh/aesh "0.44"]]
+  :main clepl/main)
